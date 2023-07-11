416a7575fc38293d4d12f995f182ca45094c7dfe
RomanChirkov2017 <138663746+RomanChirkov2017@users.noreply.github.com>
2023-07-11 23:07:11 +1000

    add new func in str_func

--- str_func.py
+++ str_func.py
@@ -2,4 +2,11 @@ def get_upper_str(user_str):
     """
     Docstring
     """
-    return user_str.upper
+    return user_str.upper()
+
+
+def get_title_str(user_str):
+    """
+    Docstring
+    """
+    return user_str.title()
